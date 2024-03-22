464e1c68971e93ea538ff16f90a43700e694b45f
NamaN Bhalani <bhalaninaman007@gmail.com>
2024-03-22 11:21:18 +0530

    Hero Section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,25 @@ import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
+  const [active, setactive] = useState('')
   return (
-    <div>Navbar</div>
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+    <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+     <Link
+         to="/"
+         className="flex items-center gap-2"
+         onClick={() => {setactive("");
+        window.scrollTo(0,0);
+      }}    
+     >
+      <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+      <p className="text-white text-[18px] font-bold cursor-pointer"> NamaN <span className="sm:block hidden">| Software Developer</span></p>
+     </Link>
+     <p className="text-red-500">kndsnfn</p>
+    </div>
+    </nav>
   )
 }
 
